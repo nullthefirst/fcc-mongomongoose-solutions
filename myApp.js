b22fa154eb8b918a01b5adcd10dbe9409bede89b
Ninte <ninte.dev@gmail.com>
2021-08-03 18:47:40 +0100

    Simplified mongoose connection.

--- myApp.js
+++ myApp.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
-  dbName: process.env.MONGO_DBNAME,
 });
 
 const db = mongoose.connection;
