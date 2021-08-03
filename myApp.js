b3f8a33243a032860b3714356cc256b9ffb35b6e
Ninte <ninte.dev@gmail.com>
2021-08-03 18:42:41 +0100

    Added extra parameters to mongoose connection.

--- myApp.js
+++ myApp.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
+  dbName: process.env.MONGO_DBNAME,
 });
 
 const db = mongoose.connection;
