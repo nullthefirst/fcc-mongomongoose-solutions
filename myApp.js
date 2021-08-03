e74157e4aa36ddcada18ae93b9d0dd1ed9b73082
Ninte <ninte.dev@gmail.com>
2021-08-03 18:36:21 +0100

    Added mongoose connection check.

--- myApp.js
+++ myApp.js
@@ -6,6 +6,12 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 });
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
 let Person;
 
 const createAndSavePerson = (done) => {
