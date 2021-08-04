daabfe4bb1a5782c271d75a60c000033c55c8a5f
Ninte <ninte.dev@gmail.com>
2021-08-04 08:22:42 +0100

    Added functionality to remove person instance by id.

--- myApp.js
+++ myApp.js
@@ -115,7 +115,11 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, function (err, data) {
+    if (err) done(err);
+
+    done(null, data);
+  });
 };
 
 const removeManyPeople = (done) => {
