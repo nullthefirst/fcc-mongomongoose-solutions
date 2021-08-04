f664c1a5100fd428d09736f534c889d25061b59a
Ninte <ninte.dev@gmail.com>
2021-08-04 08:17:47 +0100

    Added functionality to find person instance by name and update age to set variable value.

--- myApp.js
+++ myApp.js
@@ -102,7 +102,16 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    { name: personName },
+    { age: ageToSet },
+    { new: true },
+    function (err, data) {
+      if (err) done(err);
+
+      done(null, data);
+    },
+  );
 };
 
 const removeById = (personId, done) => {
