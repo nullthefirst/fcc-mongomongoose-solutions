3213d6807172d72a4190dbada59e658f75312142
Ninte <ninte.dev@gmail.com>
2021-08-04 08:26:26 +0100

    Added functionality to remove mutiple persons instance by criteria.

--- myApp.js
+++ myApp.js
@@ -125,7 +125,11 @@ const removeById = (personId, done) => {
 const removeManyPeople = (done) => {
   const nameToRemove = 'Mary';
 
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove }, function (err, data) {
+    if (err) done(err);
+
+    done(null, data);
+  });
 };
 
 const queryChain = (done) => {
