1601070a76f222d7921ae81e07f792e6dca867fd
Ninte <ninte.dev@gmail.com>
2021-08-04 07:37:46 +0100

    Added functionality to create many people from array of person documents.

--- myApp.js
+++ myApp.js
@@ -46,7 +46,13 @@ const createAndSavePerson = (done) => {
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, function (err, data) {
+    if (err) {
+      done(err);
+    }
+
+    done(null, data);
+  });
 };
 
 const findPeopleByName = (personName, done) => {
