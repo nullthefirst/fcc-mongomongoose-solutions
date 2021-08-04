ffcf4c5628f5fe315ab366aa5ac49d078843e879
Ninte <ninte.dev@gmail.com>
2021-08-04 08:35:41 +0100

    Added search by query chain functionality.

--- myApp.js
+++ myApp.js
@@ -135,7 +135,15 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = 'burrito';
 
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort('name')
+    .limit(2)
+    .select(['name', 'favoriteFoods'])
+    .exec(function (err, data) {
+      if (err) done(err);
+
+      done(null, data);
+    });
 };
 
 /** **Well Done !!**
