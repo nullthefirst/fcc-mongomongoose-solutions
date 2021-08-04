24785a57364a5b2971bd10e54f518a22028cf281
Ninte <ninte.dev@gmail.com>
2021-08-04 07:42:08 +0100

    Added functionality to find person instance from model by name argument.

--- myApp.js
+++ myApp.js
@@ -56,7 +56,13 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, function (err, data) {
+    if (err) {
+      done(err);
+    }
+
+    done(null, data);
+  });
 };
 
 const findOneByFood = (food, done) => {
